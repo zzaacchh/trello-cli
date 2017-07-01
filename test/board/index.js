530f3548a60fccc3351b802dfc46afb8d456703e
Michael Heap <m@michaelheap.com>
2017-07-01 20:05:58 +0100

    New command: board:add

---
+++ test/board/index.js
@@ -0,0 +1,73 @@
+const sinon = require("sinon");
+const sinonTest = require("sinon-test")(sinon);
+
+const chai = require("chai");
+const sinonChai = require("sinon-chai");
+chai.use(sinonChai);
+
+const expect = chai.expect;
+const program = require("../../bin/trello");
+
+describe("board", function() {
+  describe(":add", function() {
+    const board = require("../../src/board/index");
+
+    it(
+      "should pass through the board name",
+      sinonTest(function() {
+        const stub = this.stub(board, "add");
+        execute(["board:add", "fiora"]);
+
+        const args = stub.args[0];
+        expect(stub).to.have.been.calledOnce;
+        expect(args[0]).to.equal("fiora");
+      })
+    );
+
+    it(
+      "has sane defaults",
+      sinonTest(function() {
+        const stub = this.stub(board, "add");
+        execute(["board:add", "something"]);
+
+        const args = stub.args[0];
+        expect(args[1].description).to.equal(null);
+        expect(args[1].ageing).to.equal("off");
+        expect(args[1].images).to.equal("on");
+      })
+    );
+
+    [
+      ["can set description", "d", "description", "The Grand Duelist"],
+      ["can enable card aging", "a", "ageing", "on"],
+      ["can disable card images", "i", "images", "off"]
+    ].forEach(function(v) {
+      it(
+        `${v[0]} (-${v[1]})`,
+        sinonTest(function() {
+          const stub = this.stub(board, "add");
+          execute(["board:add", "test", "-" + v[1], v[3]]);
+
+          const args = stub.args[0];
+          expect(args[1][v[2]]).to.equal(v[3]);
+        })
+      );
+
+      it(
+        `${v[0]} (--${v[2]})`,
+        sinonTest(function() {
+          const stub = this.stub(board, "add");
+          execute(["board:add", "test", "--" + v[2], v[3]]);
+
+          const args = stub.args[0];
+          expect(args[1][v[2]]).to.equal(v[3]);
+        })
+      );
+    });
+  });
+});
+
+function execute(args) {
+  args.unshift("node", "trello");
+  program(args);
+}
