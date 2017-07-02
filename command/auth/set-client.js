b3d02f8742c7bf397ba69ec90f0d00e2e1dbb74c
Michael Heap <m@michaelheap.com>
2017-07-02 15:24:33 +0100

    Missed some files

--- command/auth/set-client.js
+++ command/auth/set-client.js
@@ -2,6 +2,7 @@ let config = require("../../src/config");
 
 module.exports = function(token) {
   config.writeConfigValue("auth.clientId", token);
+  config.writeConfigValue("auth.token", "");
   console.log("Client ID set");
   try {
     config.ensureAuthTokenSet();
--- test/auth.js
+++ test/auth.js
@@ -6,20 +6,62 @@ const sinonChai = require("sinon-chai");
 chai.use(sinonChai);
 
 const expect = chai.expect;
+const config = require("../src/config");
+const auth = require("../command/auth/index");
 
 describe("auth", function() {
-  const cache = require("../src/cache");
-
-  describe("#check", function() {
+  describe("#set-client", function() {
     it(
-      "should call console.log",
+      "should write the ID to config",
       sinonTest(function() {
-        const stub = this.stub(console, "log");
-        cache.refreshOrgs();
+        const writeConfigStub = this.stub(config, "writeConfigValue");
+        const authTokenStub = this.stub(config, "ensureAuthTokenSet");
+        const consoleStub = this.stub(console, "log");
 
-        const args = stub.args[0];
-        expect(stub).to.have.been.calledOnce;
-        expect(args[0]).to.equal("Building org cache");
+        auth.setClient("client_id");
+
+        expect(writeConfigStub).to.have.been.calledTwice;
+
+        // First we write the client ID
+        const writeConfigArgs = writeConfigStub.args[0];
+        expect(writeConfigArgs[0]).to.equal("auth.clientId");
+        expect(writeConfigArgs[1]).to.equal("client_id");
+
+        // Then we clear the auth token as it's a new client ID
+        const writeTokenArgs = writeConfigStub.args[1];
+        expect(writeTokenArgs[0]).to.equal("auth.token");
+        expect(writeTokenArgs[1]).to.equal("");
+
+        // Then tell the user it's been written
+        const consoleArgs = consoleStub.args[0];
+        expect(consoleStub).to.have.been.calledOnce;
+        expect(consoleArgs[0]).to.equal("Client ID set");
+
+        // Finally, trigger the auth token error to tell the user
+        // to generate a new token
+        expect(authTokenStub).to.have.been.calledOnce;
+      })
+    );
+  });
+  describe("#set-token", function() {
+    it(
+      "should write the token to config",
+      sinonTest(function() {
+        const writeConfigStub = this.stub(config, "writeConfigValue");
+        const consoleStub = this.stub(console, "log");
+
+        auth.setToken("myToken");
+
+        // First we write the client ID
+        const writeConfigArgs = writeConfigStub.args[0];
+        expect(writeConfigStub).to.have.been.calledOnce;
+        expect(writeConfigArgs[0]).to.equal("auth.token");
+        expect(writeConfigArgs[1]).to.equal("myToken");
+
+        // Then tell the user it's been written
+        const consoleArgs = consoleStub.args[0];
+        expect(consoleStub).to.have.been.calledOnce;
+        expect(consoleArgs[0]).to.equal("Auth token set");
       })
     );
   });
