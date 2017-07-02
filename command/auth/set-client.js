a60676752bfbd11c937e9ba236d7efdc546538f0
Michael Heap <m@michaelheap.com>
2017-07-02 15:13:06 +0100

    Better getting started flow

--- command/auth/set-client.js
+++ command/auth/set-client.js
@@ -2,4 +2,10 @@ let config = require("../../src/config");
 
 module.exports = function(token) {
   config.writeConfigValue("auth.clientId", token);
+  console.log("Client ID set");
+  try {
+    config.ensureAuthTokenSet();
+  } catch (e) {
+    console.log(e.message);
+  }
 };
--- command/auth/set-token.js
+++ command/auth/set-token.js
@@ -2,4 +2,5 @@ let config = require("../../src/config");
 
 module.exports = function(token) {
   config.writeConfigValue("auth.token", token);
+  console.log("Auth token set");
 };
